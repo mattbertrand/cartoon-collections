11689308027799e2b6c0b6d5ee9514876c9801fa
github username <github email address>
2020-11-14 11:27:04 -0800

    commit tree b3269e8c2068e10d9bf255d2bb49e672b31d6cd3

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,36 @@
-def roll_call_dwarves# code an argument here
+dwarves = %w[Dopey Doc Grumpy Happy Bashful Sneezy Sleepy]
+
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do|dwarves, index|
+    puts "#{index +1}. #{dwarves}"
+end
+  # code an argument here
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+planeteer = %w[Wind Earth Fire Water Heart]
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map {|call| call.capitalize + "!"}
+  # code an argument here
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? {|call| call.length > 4}
+  # code an argument here
   # Your code here
 end
 
-def find_the_cheese# code an argument here
+potential_cheese = %w[banana cheddar sock]
+
+def find_the_cheese(potential_cheese)
+  cheeses = %w[gouda cheddar camembert]
+
+  potential_cheese.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end
+  # code an argument here
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
